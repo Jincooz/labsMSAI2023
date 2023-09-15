0bb7ebfef2c2dbb29c6ffda8e3b036392f5d95ad
Vitalii <shaddyreaper@gmail.com>
2023-09-15 12:13:22 +0300

    Rethink Map and define methods in State

--- lab1/main.py
+++ lab1/main.py
@@ -1,9 +1,31 @@
+from enum import Enum
 
+class DoorColor(Enum):
+    RED = 1 #door to exit
+    GREEN = 2 #treasure nrae this room
+    BLUE = 3 #nothing special
+
+class CardinalDirections(Enum):
+    RIGHT = (1, 0)
+    LEFT = (-1, 0)
+    UP = (0, 1)
+    DOWN = (0, -1)
 
 class Position:
+
     def __init__(self, x, y) -> None:
         self.x = x
         self.y = y
+
+    def __eq__(self, __value: object) -> bool:
+        return self.x == __value.x and self.y == __value.y
+    
+    def __add__(self, other : CardinalDirections):
+        return Position(self.x + other.value[0], self.y + other.value[1])
+    
+    def __str__(self) -> str:
+        return "(" + str(self.x) + ", " + str(self.y) + ")" 
+    
 
 class Map:
     def __init__(self, map_width, map_height, exit_positions, treasure_positions) -> None:
@@ -27,78 +49,67 @@ class Map:
                         break
                 if(curent_position_is_empty):
                     self.empty_positions.append(Position(x,y))
-    
-    def get_posible_treasure_positions(self):
+        
+        self.door_map = [[DoorColor.BLUE for x in range(self.map_width)] for y in range(self.map_height)]
+        for treasure_poition in self.treasure_positions:
+            for x in range(max(treasure_poition.x - 1, 0), min(treasure_poition.x + 1, map_width)):
+                for y in range(max(treasure_poition.y - 1, 0), min(treasure_poition.y + 1, map_height)):
+                    self.door_map[x][y] = DoorColor.GREEN
+            self.door_map[treasure_poition.x][treasure_poition.y] = DoorColor.GREEN
+        for exit_position in self.exit_positions:
+            self.door_map[exit_position.x][exit_position.y] = DoorColor.BLUE
+
+    def get_treasure_positions(self):
         return self.treasure_positions
+
+    def is_treasure_position(self, position):
+        return position in self.treasure_positions
 
     def get_exit_poitions(self):
         return self.exit_positions
     
     def get_empty_positions(self):
         return self.empty_positions
+    
+    def get_door_color(self, from_position : Position, to_position : Position):
+        return self.door_map[to_position.x][to_position.y]
 
-    def discover_position(self, position):
-        pass
-
-    def get_posible_actions(self, agent_position):
+    def get_posible_actions(self, agent_position) -> list(CardinalDirections):
         posible_moves = []
         if(agent_position.x != 0):
-            posible_moves.append("L")
+            posible_moves.append(CardinalDirections.LEFT)
         if(agent_position.x != self.map_width - 1):
-            posible_moves.append("R")
+            posible_moves.append(CardinalDirections.RIGHT)
         if(agent_position.y != self.map_height - 1):
-            posible_moves.append("U")
+            posible_moves.append(CardinalDirections.UP)
         if(agent_position.y != 0):
-            posible_moves.append("D")
+            posible_moves.append(CardinalDirections.DOWN)
         return posible_moves
 
-class UndiscoveredMap(Map):
-    def __init__(self, map_width, map_height, exit_positions, treasure_positions) -> None:
-        Map.__init__(self, map_width, map_height, exit_positions, treasure_positions)
-        self.discovered_exit_positions = []
-        self.discovered_empty_positions = []
-        self.discovered_posible_treasure_positions = []
-        self.discovered_positions = []
+class Agent:
+    def __init__(self, agent_position : Position) -> None:
+        self.map_position = agent_position
     
-    def get_posible_treasure_positions(self):
-        return self.discovered_posible_treasure_positions
-
-    def get_exit_poitions(self):
-        return self.discovered_exit_positions
-    
-    def get_empty_positions(self) -> list(Position):
-        return self.discovered_empty_positions
-
-    def discover_position(self, agent_position) -> None:
-        #TODO: write this method
-        for discovered_position in self.discovered_positions:
-            if(discovered_position.x == agent_position.x and discovered_position.y == agent_position.y):
-                return
-        self.discovered_positions.append(Position(agent_position.x,agent_position.y))
-        x_range = range(max(0, agent_position.x - 2), min(agent_position.x + 2, self.map_width))
-        y_range = range(max(0, agent_position.y - 2), min(agent_position.y + 2, self.map_height))
-        nearby_map = [[]]
-        for x in x_range:
-            for y in y_range:
-                pass
-
+    def get_position(self):
+        return self.map_position
 
 class State:
-    def __init__(self, agent_position, map) -> None:
-        self.agent_position = agent_position
+    def __init__(self, agent : Agent, map : Map) -> None:
+        self.agent = agent
         self.map = map
 
     def posible_actions(self):
-        pass
+        return self.map.get_posible_actions(self.agent.map_position)
     
     def is_final(self):
-        pass
+        return self.map.is_treasure_position(self.agent.map_position)
 
-    def move(self, action):
-        pass
+    def move(self, action : CardinalDirections):
+        self.agent.map_position += action
 
 def main():
-    pass
+    p = Position(0, 0)
+    print(p + CardinalDirections.UP)
 
 if (__name__ == '__main__'):
     main()
